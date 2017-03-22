2f53c8350532ca7a055d33a9d79b8a0c82e0d45d
Juan2Ramos <juan2ramos@gmail.com>
2017-03-21 22:54:33 -0500

    style contact

--- wp-content/themes/petpet/header.php
+++ wp-content/themes/petpet/header.php
@@ -54,7 +54,7 @@
                       id="petpet" fill="#FFFFFF"></path>
             </g>
         </svg>
-        <ul class=" row">
+        <ul class="  row center">
             <li>
                 <a href="https://www.facebook.com/petpet.comunidad">
                 <svg width="26px" height="26px" viewBox="0 0 26 26" version="1.1" xmlns="http://www.w3.org/2000/svg"
